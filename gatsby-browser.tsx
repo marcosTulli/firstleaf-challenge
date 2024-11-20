25227ac0ec251c6e410278ac856f4f23fc92c7a2
Marcos <marcostulli@hotmail.com>
2024-11-19 23:30:14 -0300

    Add global style

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import * as React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import './src/styles/global.css';
 
 const queryClient = new QueryClient();
 
