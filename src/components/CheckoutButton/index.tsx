be963c4ee39db5dede1804f0c2c8fabc515d6872
Marcos <marcostulli@hotmail.com>
2024-11-19 23:18:39 -0300

    Use variable

--- src/components/CheckoutButton/index.tsx
+++ src/components/CheckoutButton/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import * as React from 'react';
 import * as styles from './index.module.scss';
 
-const CheckoutButton = () => {
+const CheckoutButton: React.FC = () => {
     return (
         <button className={styles.button} >Checkout</button>
     );
--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,9 +1,10 @@
 import { useQuery } from 'react-query';
 import { Product } from '../types/Products';
 
+const PRODUCTS_API = process.env.GATSBY_PRODUCTS_API || '';
 const useProducts = () => {
   const { data, isLoading } = useQuery('products', async () => {
-    const products = await fetch('https://my-json-server.typicode.com/YofretRios/jsondetails/products')
+    const products = await fetch(PRODUCTS_API)
       .then(res => res.json()) as Array<Product>;
 
     window.localStorage.setItem('products', JSON.stringify(products));
