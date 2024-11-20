81a4e7fbe43ce1e6d48adde908f425573a29a131
Marcos <marcostulli@hotmail.com>
2024-11-19 22:52:25 -0300

    Add checkout button

---
+++ src/components/CheckoutButton/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as styles from './index.module.scss';
+
+const CheckoutButton = () => {
+    return (
+        <button className={styles.button} >Checkout</button>
+    );
+};
+
+export default CheckoutButton;
